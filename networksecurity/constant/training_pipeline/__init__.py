b30e191476b71adb738a3fa3d84804904e7ace57
tanyaagrawalhii <nat.agra7@gmail.com>
2025-05-28 17:10:49 +0530

    data validation completed

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -14,7 +14,7 @@ FILE_NAME: str = "phisingData.csv"
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 
-# SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
+SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
 
 # SAVED_MODEL_DIR =os.path.join("saved_models")
 # MODEL_FILE_NAME = "model.pkl"
@@ -32,15 +32,15 @@ DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
 
-# """
-# Data Validation related constant start with DATA_VALIDATION VAR NAME
-# """
-# DATA_VALIDATION_DIR_NAME: str = "data_validation"
-# DATA_VALIDATION_VALID_DIR: str = "validated"
-# DATA_VALIDATION_INVALID_DIR: str = "invalid"
-# DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
-# DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
-# PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+"""
+Data Validation related constant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 
 # """
 # Data Transformation related constant start with DATA_TRANSFORMATION VAR NAME

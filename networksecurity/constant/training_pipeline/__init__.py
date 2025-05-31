01d43b77721aa86c5759fc131896f2bca7bbe106
tanyaagrawalhii <nat.agra7@gmail.com>
2025-05-31 23:33:09 +0530

    mlflow

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -16,8 +16,8 @@ TEST_FILE_NAME: str = "test.csv"
 
 SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
 
-# SAVED_MODEL_DIR =os.path.join("saved_models")
-# MODEL_FILE_NAME = "model.pkl"
+SAVED_MODEL_DIR =os.path.join("saved_models")
+MODEL_FILE_NAME = "model.pkl"
 
 
 
@@ -60,14 +60,14 @@ DATA_TRANSFORMATION_TRAIN_FILE_PATH: str = "train.npy"
 DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
 
 
-# """
-# Model Trainer ralated constant start with MODE TRAINER VAR NAME
-# """
+"""
+Model Trainer ralated constant start with MODE TRAINER VAR NAME
+"""
 
-# MODEL_TRAINER_DIR_NAME: str = "model_trainer"
-# MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
-# MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
-# MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
-# MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD: float = 0.05
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD: float = 0.05
 
-# TRAINING_BUCKET_NAME = "netwworksecurity"
+TRAINING_BUCKET_NAME = "netwworksecurity"
